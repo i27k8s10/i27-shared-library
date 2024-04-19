09083013152ce663b86f6eee90c35fa9cda4c8f2
i27k8s10 <157795024+i27k8s10@users.noreply.github.com>
2024-04-19 07:16:03 +0530

    1904

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -1,19 +1,25 @@
 import com.i27academy.builds.Calculator
-Calculator calculator = new Calculator(this)
-pipeline {
-    agent any 
-    stages {
-        stage ('AdditionStage') {
-            steps {
-                script {
-                    echo "Printing sum of 2 numbers"
-                    println calculator.add(10,20)
+def call(Map pipelineParams){
+    Calculator calculator = new Calculator(this)    
+    pipeline {
+        agent any 
+        environment {
+            APP_NAME = "${pipelineParams.appName}"
+        }
+        stages {
+            stage ('AdditionStage') {
+                steps {
+                    script {
+                        echo "Printing sum of 2 numbers"
+                        println calculator.add(10,20)
+                        echo "My MicroService Name is: ${APP_NAME}"
+                    }
                 }
             }
-        }
-        stage ('Mailing') {
-            steps {
-                echo "Sending a mail !!!!!!!!!!!!"
+            stage ('Mailing') {
+                steps {
+                    echo "Sending a mail !!!!!!!!!!!!"
+                }
             }
         }
     }
